d91372a9a9c3393421c7495e2cd05f7d126abdbe
Mansi Barodia <mansibarodia@MacBook-Pro.local>
2016-12-03 06:48:23 +0530

    Creating new API

---
+++ Emergency-Numbers-API/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EmergencyNumbersApi::Application.config.secret_key_base = 'e837032c867c6c5cea97261eb443ae07f848c7ece1abc0e4e0bff4ad788b1fa2b6aa9dbc789780e7c2646e03d5d13dd68ef17ed4aff1c81e4307d7d549124dbe'
---
+++ Emergency-Numbers-API/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EmergencyNumbersApi::Application.config.session_store :cookie_store, key: '_Emergency-Numbers-API_session'
